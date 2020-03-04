049a6ada5804e948add0e6edf0a917a67a38d150
Rafael de Mesquita Moura <rafael.mesquita@softcube.com.br>
2020-03-03 22:01:15 -0300

    Inclusão da anotação que faltou

--- src/main/java/br/com/softcube/expensemanagement/configuration/ExpenseManagementWebConfiguration.java
+++ src/main/java/br/com/softcube/expensemanagement/configuration/ExpenseManagementWebConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.softcube.expensemanagement.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ExpenseManagementWebConfiguration {
 
+    @Bean
     public WebMvcConfigurer corsConfiguration() {
         return new WebMvcConfigurer() {
             @Override
