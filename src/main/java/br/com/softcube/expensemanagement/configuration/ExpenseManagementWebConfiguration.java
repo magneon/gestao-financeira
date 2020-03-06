6391c33e0ea6052eb5f82bd50cf9de356074813f
Rafael de Mesquita Moura <rafael.mesquita@softcube.com.br>
2020-03-06 16:23:14 -0300

    Enabling CORS

--- src/main/java/br/com/softcube/expensemanagement/configuration/ExpenseManagementWebConfiguration.java
+++ src/main/java/br/com/softcube/expensemanagement/configuration/ExpenseManagementWebConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ExpenseManagementWebConfiguration {
 
-    @Bean
+    //@Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
